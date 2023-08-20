bd88ad39c8a04a250d3101480b9d58e72b154b10
Manish Prajapati <manishprajapati@gofynd.com>
2023-08-20 16:48:23 +0530

    Set local and container commands

---
+++ run.local.sh
@@ -0,0 +1,5 @@
+export NODE_ENV=dev
+export MONGO_INITDB_ROOT_USERNAME=admin
+export MONGO_INITDB_ROOT_PASSWORD=password
+
+npm run dev
--- services/index.js
+++ services/index.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-const connectionURILocal = 'mongodb://admin:password@127.0.0.1:27017'
-
-const connectionURIContainer = 'mongodb://admin:password@mongodb'
+// run docker mongo image with username and password passed in env and named as mongodb
+const db_username = process.env.MONGO_INITDB_ROOT_USERNAME
+const db_password = process.env.MONGO_INITDB_ROOT_PASSWORD
+// while using docker compose, we can directly use service name
+// while runing project locally, we need to provide local uri of mongo db
+const serviceURI = process.env.NODE_ENV === "dev" ? "127.0.0.1:27017" : "mongodb"
+const connectionURI = `mongodb://${db_username}:${db_password}@${serviceURI}`
 
 const mongoInit = () =>
   new Promise((res, rej) => {
     mongoose
-      .connect(connectionURIContainer)
+      .connect(connectionURI)
       .then((m) => {
         console.log('DB Connected');
         res();
